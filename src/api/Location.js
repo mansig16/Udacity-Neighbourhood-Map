869b6a41f6768063c76fc8eaed151fe6fa4e4372
mansig <mansig16551@gmail.com>
2018-12-25 21:57:37 +0530

    Update Location.js

--- src/api/Location.js
+++ src/api/Location.js
@@ -7,7 +7,7 @@ function handleErrors(response) {
 
 //getting the locations using fourSquare api
 export const getLocations=()=>{
-return fetch(`https://api.foursquare.com/v2/venues/explore?cat=food&near=chandigarh&client_id=JYUEGMQH3K0RWJETUG0XNSX0AQ14R1RSK3HY00ILQQGEXPHX&client_secret=UCLJ3AM4YTSAPUE3LPRLWTNU3GRCMKBCLTS2C5IKU4S5HTKF&v=20182507`
+return fetch(`https://api.foursquare.com/v2/venues/explore?cat=food&near=chandigarh&client_id=""&client_secret=""&v=20182507`
 	).then(handleErrors).then(resp =>resp.json())
 .then(result =>result.response.groups[0].items)
 .catch(error => alert("Foursquare error. Try to restart the page."));
